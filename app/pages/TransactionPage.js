5bcce5543ea343f16956d48ec5162db5f1089937
MukeshThummar <mukeshnthummar@gmail.com>
2025-01-12 22:05:40 +0530

    Update Transaction Page

--- app/pages/TransactionPage.js
+++ app/pages/TransactionPage.js
@@ -1,29 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import { Text, Button, View, TouchableOpacity, FlatList, StyleSheet,ScrollView } from 'react-native';
+import { Text, Button, View, TouchableOpacity, FlatList, StyleSheet, ScrollView } from 'react-native';
 import { useNavigation, useFocusEffect } from '@react-navigation/native';
-import { addTransaction, updateTransaction, deleteTransaction, getTransactions } from  '../../services/transactionService';
+import { addTransaction, updateTransaction, deleteTransaction, getTransactions } from '../../services/transactionService';
 
 
 const TransactionPage = () => {
   const navigation = useNavigation();
   const [transactions, setTransactions] = useState([]);
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
 
-  
+
   useFocusEffect(
     React.useCallback(() => {
       const fetchData = async () => {
         try {
           const transactionsList = await getTransactions();
           setTransactions(transactionsList); // Set transactions state
-  
+
         } catch (error) {
           console.error('Error fetching vendors:', error); // Handle any errors
         } finally {
           setLoading(false);
         }
       };
-  
+
       fetchData();
     }, [])
   );
@@ -36,8 +36,10 @@ const TransactionPage = () => {
       <Text style={styles.gridcols}>{item.rate}</Text>
       <Text style={styles.gridcols}>{item.amount}</Text>
       <Text style={styles.gridcols}>{item.description}</Text>
-      <Button title="Edit" onPress={() => console.log('Edit transaction')} />
-      <Button title="Delete" onPress={() => console.log('Delete transaction')} />
+      <View style={styles.actions}>
+        <Button title="Edit" onPress={() => console.log('Edit transaction')} />
+        <Button title="Delete" onPress={() => console.log('Delete transaction')} />
+      </View>
     </View>
   );
 
@@ -45,35 +47,35 @@ const TransactionPage = () => {
     <View style={styles.container}>
       <Text style={styles.input}>Transactions</Text>
       {transactions === undefined || transactions.length === 0 ? (
-            <Text style={styles.input}>No transactions.</Text>
-          ) : (
-            <ScrollView horizontal>
-              <View>
-                {/* Table Header */}
-                <View style={styles.grid}>
-                  <Text style={styles.gridheader}>Vendor</Text>
-                  <Text style={styles.gridheader}>Date</Text>
-                  <Text style={styles.gridheader}>Quantity</Text>
-                  <Text style={styles.gridheader}>Rate</Text>
-                  <Text style={styles.gridheader}>Amount</Text>
-                  <Text style={styles.gridheader}>Description</Text>
-                </View>
+        <Text style={styles.input}>No transactions.</Text>
+      ) : (
+        <ScrollView horizontal>
+          <View>
+            {/* Table Header */}
+            <View style={styles.grid}>
+              <Text style={styles.gridheader}>Vendor</Text>
+              <Text style={styles.gridheader}>Date</Text>
+              <Text style={styles.gridheader}>Quantity</Text>
+              <Text style={styles.gridheader}>Rate</Text>
+              <Text style={styles.gridheader}>Amount</Text>
+              <Text style={styles.gridheader}>Description</Text>
+            </View>
 
-                {/* Table Data */}
-                <FlatList
-                  data={transactions}
-                  keyExtractor={(item) => item.transactionId.toString()}
-                  renderItem={renderTransaction} // Render each transaction
-                />
-              </View>
-            </ScrollView>
-          )}
-          <TouchableOpacity
-          style={styles.addButton}
-          onPress={() => navigation.navigate('TransactionAdd')}
-        >
-          <Text style={styles.addButtonText}>+</Text>
-        </TouchableOpacity>
+            {/* Table Data */}
+            <FlatList
+              data={transactions}
+              keyExtractor={(item) => item.transactionId.toString()}
+              renderItem={renderTransaction} // Render each transaction
+            />
+          </View>
+        </ScrollView>
+      )}
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={() => navigation.navigate('TransactionAdd')}
+      >
+        <Text style={styles.addButtonText}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -129,17 +131,17 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     borderBottomWidth: 1,
     paddingBottom: 5,
-    paddingVertical: 10 
+    paddingVertical: 10
   },
   gridheader: {
-    flex: 1 ,
+    flex: 1,
     fontWeight: 'bold',
     fontSize: 16,
     lineHeight: 24,
     color: '#ffffff',
   },
   gridcols: {
-    flex: 1 ,
+    flex: 1,
     fontSize: 16,
     lineHeight: 24,
     color: '#ffffff',
